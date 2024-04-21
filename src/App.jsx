64dfd20f0eab477ec697b142abf9a655bf564055
Vishal Pandey <igvishalpandey@gmail.com>
2024-04-21 20:54:49 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth'
+import {login, logout} from './store/authSlice'
+import {Header, Footer} from './components'
+import {Outlet} from 'react-router-dom'  
 
 function App() {
 
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading]= useState(true);
+  const dispatch= useDispatch();
 
-  return (
-    <div>App</div>
-  )
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      } else{
+        dispatch(logout());
+      }
+    })
+    .finally(()=>setLoading(false));
+  },[])
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-zinc-900 text-white">
+      <div className='w-full block'>
+            <Header/>
+            <main>
+              <Outlet />
+            </main>
+            <Footer/>
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App
